d09c9326fbde89aaca173f0e311fed0160280fcb
Ben Mesirow <bmezzy92@gmail.com>
2013-09-19 12:55:59 -0400

    First Commit!
    Nothing too interesting yet

---
+++ script.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+
+  /*
+  function NavOption(id, hoverColor) {
+    this.htmlObject = $(id);
+    this.hoverColor = hoverColor;
+  }
+
+  var home     = NavOption.new("#home",     "#ff4000");
+  var projects = NavOption.new("#projects", "#40ff80");
+  var contact  = NavOption.new("#contact",  "#ffb000");
+  var about    = NavOption.new("#about",    "#6058ff");
+  */
+  /*
+    #home", "#ff4000",
+    "#projects", "#40ff80",
+    "#contact", "#ffb000",
+    "#about", "#6058ff"
+  */
+  var currSelected = $("#home");
+  /*$(".navbar div").mouseenter(function() {
+    currSelected.(function() {
+      $("#home").css("color","");
+    });
+    alert("YOU JUST CLICKED " + this);
+    //currSelected.css("color", "#FFFFFF");
+    //$(this).css("color", hoverColor[$(this)]);
+    currSelected = $(this);
+  });*/
+});
